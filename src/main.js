210a6eab476ca5e9ec28f0d78db28b6aa3575552
sshwy <jy.cat@qq.com>
2022-07-30 16:26:26 +0800

    feat: i18n

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import router from './Router.js'
 import App from './App.vue'
 import { callRPC } from './utils.js'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import './css/yaoj.css'
+
+import messages from "./i18n"
+
+const i18n = createI18n({
+    locale: 'zh',
+    fallbackLocale: 'en',
+    messages: messages,
+})
 
 callRPC("Init", {}, function(response) {
     const app = createApp(App)
@@ -14,6 +23,7 @@ callRPC("Init", {}, function(response) {
     app.config.compilerOptions.isCustomElement = (tag) => tag.startsWith("ion")
     app.config.compilerOptions.jsx = "preserve"
     app.use(router)
+    app.use(i18n)
     app.mount("#app")
 }, function(response) {
     alert(response.statusMessage + "\n" + response.data._error.message)
