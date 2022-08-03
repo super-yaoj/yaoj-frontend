3d41bf8a6662553ad3fd94a5684050de235fc392
sshwy <jy.cat@qq.com>
2022-08-03 21:23:46 +0800

    refactor: sidebar

--- src/TheSidebar.vue
+++ src/TheSidebar.vue
@@ -1,116 +1,68 @@
 <template>
-    <nav
-        id="sidebar-menu"
-        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-        style="min-width: 180px;"
-    ><div class="sidebar-sticky">
-        <ul class="nav flex-column">
-            <ni-i18n icon="file-tray-full-outline" t="sidebar.problems" href="/problems" />
-            <ni-i18n icon="hourglass-outline" t="sidebar.submissions" href="/submissions" />
-            <ni-i18n icon="bar-chart-outline" t="sidebar.contests" href="/contests" />
-            <ni-i18n icon="create-outline" t="sidebar.blogs" href="/blogs" />
-            <li class="d-flex justify-content-between">
-                <ni-i18n
-                    class="w-100"
-                    icon="refresh-outline"
-                    t="sidebar.refresh"
-                    @click.prevent="$props.reload"
-                    :href="$route.fullPath"
-                />
-                <div
-                    v-if="$props.refreshed"
-                    style="height: 100%; padding-top: 10px"
-                    class="pe-3"
-                >
-                    <ion-icon
-                        name="checkmark-circle-outline"
-                        style="font-size: 1.1rem; color: green"
-                    />
+    <nav id="sidebar-menu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" style="min-width: 180px;">
+        <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+                <ni-i18n icon="file-tray-full-outline" t="sidebar.problems" href="/problems" />
+                <ni-i18n icon="hourglass-outline" t="sidebar.submissions" href="/submissions" />
+                <ni-i18n icon="bar-chart-outline" t="sidebar.contests" href="/contests" />
+                <ni-i18n icon="create-outline" t="sidebar.blogs" href="/blogs" />
+                <li class="d-flex justify-content-between">
+                    <ni-i18n class="w-100" icon="refresh-outline" t="sidebar.refresh" @click.prevent="$props.reload"
+                        :href="$route.fullPath" />
+                    <div v-if="$props.refreshed" style="height: 100%; padding-top: 10px" class="pe-3">
+                        <ion-icon name="checkmark-circle-outline" style="font-size: 1.1rem; color: green" />
+                    </div>
+                </li>
+            </ul>
+            <hr class="mx-2" />
+            <div v-if="$store.user.user_id > 0">
+                <div>
+                    <span class="col align-items-center px-3 text-muted"
+                        style="text-transform: uppercase; color: #444">{{ $store.user.user_name }}</span>
                 </div>
-            </li>
-        </ul>
-        <hr class="mx-2" />
-        <div v-if="$store.user.user_id > 0">
-            <div>
-                <span
-                    class="col align-items-center px-3 text-muted"
-                    style="text-transform: uppercase; color: #444"
-                    >{{ $store.user.user_name }}</span
-                >
+                <div id="userForm" class="mt-2">
+                    <ul class="nav flex-column">
+                        <ni-i18n icon="newspaper-outline" t="sidebar.profile" :href="'/user/' + $store.user.user_id" />
+                        <ni-i18n icon="pulse-outline" t="sidebar.custom_test" href="/customtest" />
+                        <ni-i18n icon="build-outline" t="sidebar.system_manage" href="/manage" v-if="isAdmin()" />
+                        <ni-i18n icon="keypad-outline" t="sidebar.permissions" href="/permissions" v-if="isAdmin()" />
+                        <ni-i18n icon="exit-outline" t="sidebar.logout" href="/logout" />
+                    </ul>
+                </div>
             </div>
-            <div id="userForm" class="mt-2">
-                <ul class="nav flex-column">
-                    <ni-i18n
-                        icon="newspaper-outline"
-                        t="sidebar.profile"
-                        :href="'/user/' + $store.user.user_id"
-                    />
-                    <ni-i18n
-                        icon="pulse-outline"
-                        t="sidebar.custom_test"
-                        href="/customtest"
-                    />
-                    <ni-i18n
-                        icon="build-outline"
-                        t="sidebar.system_manage"
-                        href="/manage"
-                        v-if="isAdmin()"
-                    />
-                    <ni-i18n
-                        icon="keypad-outline"
-                        t="sidebar.permissions"
-                        href="/permissions"
-                        v-if="isAdmin()"
-                    />
-                    <ni-i18n
-                        icon="exit-outline"
-                        t="sidebar.logout"
-                        href="/logout"
-                    />
-                </ul>
-            </div>
-        </div>
-        <div v-else>
-            <div><h6
-                class="
+            <div v-else>
+                <div>
+                    <h6 class="
                     d-flex justify-content-between
                     align-items-center px-3 mt-2 mb-1 text-muted
-                "
-                style="text-transform: uppercase"
-            >
-                <span style="color: #444">please login</span>
-            </h6></div>
-            <ul class="nav flex-column mt-2">
-                <ni-i18n
-                    icon="enter-outline"
-                    t="sidebar.login"
-                    href="/login"
-                />
-                <ni-i18n
-                    icon="person-add-outline"
-                    t="sidebar.signup"
-                    href="/signup"
-                />
-            </ul>
-        </div>
-        <hr class="mx-2" />
-        <div class="px-3">
-            <div class="row" style="text-align: center">
-                <div class="px-3 col small" style="color: gray">
-                    Yao Online Judge
-                    <br />
-                    Server time: {{ $props.servertime }}
+                " style="text-transform: uppercase">
+                        <span style="color: #444">please login</span>
+                    </h6>
+                </div>
+                <ul class="nav flex-column mt-2">
+                    <ni-i18n icon="enter-outline" t="sidebar.login" href="/login" />
+                    <ni-i18n icon="person-add-outline" t="sidebar.signup" href="/signup" />
+                </ul>
+            </div>
+            <hr class="mx-2" />
+            <div class="px-3">
+                <div class="row" style="text-align: center">
+                    <div class="px-3 col small" style="color: gray">
+                        Yao Online Judge
+                        <br />
+                        Server time: {{ $props.servertime }}
+                    </div>
+                </div>
+                <div class="locale-changer mt-3">
+                    <select v-model="$i18n.locale" class="form-select" @change="onChangeLocale($event)">
+                        <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{
+                                $i18n.getLocaleMessage(locale).name
+                        }}</option>
+                    </select>
                 </div>
             </div>
-            <div class="locale-changer mt-3">
-                <select v-model="$i18n.locale" class="form-select" @change="onChangeLocale($event)">
-                    <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ 
-                        $i18n.getLocaleMessage(locale).name
-                    }}</option>
-                </select>
-            </div>
         </div>
-    </div></nav>
+    </nav>
 </template>
 
 <script>
@@ -137,7 +89,7 @@ export default {
 </script>
 
 <style>
-#sidebar-menu ul > li:hover {
+#sidebar-menu ul>li:hover {
     background-color: rgba(0, 0, 0, 0.040);
 }
 </style>
