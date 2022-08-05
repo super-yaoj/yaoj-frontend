31e5244256fe57fd963ab6d5960ab59e91fbfad3
sshwy <jy.cat@qq.com>
2022-08-05 21:52:34 +0800

    refactor: clicklike

--- src/models/ClickLike.vue
+++ src/models/ClickLike.vue
@@ -1,49 +1,46 @@
 <template>
-    <div
-        :class="$props.class"
-        :style="
-            ($props.target == undefined ? '' : 'cursor: pointer;') +
-            (current ^ $props.active ? 'color: #F70' : '')
-        "
-        @click="like"
-    >
-        <ion-icon
-            :name="$props.icon"
-            style="vertical-align: middle; font-size: 1rem; margin-right: 2px"
-        />
-        <span style="vertical-align: middle">{{ num }}</span>
-    </div>
+  <div :style="selfstyle" @click="onClickLike">
+    <ion-icon :name="icon" style="vertical-align: middle; font-size: 1rem; margin-right: 2px" />
+    <span style="vertical-align: middle">{{ num }}</span>
+  </div>
 </template>
 
 <script>
 import { callAPI } from "@/utils";
 
 export default {
-    props: ["icon", "number", "active", "target", "class"],
-    data() {
-        return { current: false };
-    },
-    methods: {
-        like() {
-            if (this.target == undefined) return;
-            callAPI(
-                "likes",
-                "post",
-                { target: this.target.name, id: this.target.id },
-                (res) => {
-                    this.current = !this.current;
-                },
-                (res) => {
-                    alert(res.data._error);
-                }
-            );
+  props: ["icon", "number", "active", "target"],
+  data() {
+    return {
+      current: false,
+    };
+  },
+  methods: {
+    onClickLike() {
+      if (!this.target) return;
+      callAPI("likes", "post",
+        { target: this.target.name, id: this.target.id },
+        (res) => {
+          this.current = !this.current;
         },
+        (res) => {
+          alert(res.data._error);
+        }
+      );
     },
-    computed: {
-        num() {
-            if (this.active) return this.number + (this.current ? -1 : 0);
-            return this.number + (this.current ? 1 : 0);
-        },
+  },
+  computed: {
+    num() {
+      if (this.active) return this.number + (this.current ? -1 : 0);
+      return this.number + (this.current ? 1 : 0);
     },
+    selfstyle() {
+      return {
+        cursor: this.target && 'pointer' || undefined,
+        color: this.current ^ this.active && '#F70' || undefined,
+        userSelect: 'none',
+      }
+    },
+  },
 };
 </script>
