8263dae51a21f727603fe5b6227bde810ec7dad4
sshwy <jy.cat@qq.com>
2022-08-30 22:51:55 +0800

    refactor: login

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,53 +1,50 @@
 <template>
-<div class="text-center align-items-center d-flex h-100" style="padding-bottom:80px">
+  <div class="text-center align-items-center d-flex h-100">
     <form class="form-signin" @submit.prevent="login">
-        <h1 class="h3 mb-3 font-weight-normal" v-t="'sign.login_title'"></h1>
-        <input type="username" class="form-control form-signin-top" :placeholder="$t('sign.username')" v-model="user_name"/>
-        <input type="password" class="form-control form-signin-bottom" :placeholder="$t('sign.password')" v-model="password"/>
-        <div class="text-danger small mt-1" v-if="invalid_u">
-            Username must be made up by letters or numbers or underline and contains at least 3 at most 18 characters.
-        </div>
-        <div class="text-danger small mt-1" v-if="invalid_p">
-            Password must be made up by letters or numbers or any special characters and contains at least 6 at most 18 characters.
-        </div>
-        <div class="text-danger small mt-1" v-if="wrong_login != ''">
-            {{wrong_login}}
-        </div>
-        <div class="form-check mt-3 mb-3">
-            <input class="form-check-input" type="checkbox" value="" id="remember_me" style="float:none" v-model="remember">
-            <label class="form-check-label ms-1" for="remember_me" v-t="'sign.remember'"></label>
-        </div>
-        <button class="btn btn btn-primary btn-block" type="submit" style="width:50%" v-t="'sign.login'"></button>
+      <h1 class="h3 mb-3 font-weight-normal" v-t="'sign.login_title'"></h1>
+      <input type="username" class="form-control form-signin-top" :placeholder="$t('sign.username')"
+        v-model="user_name" />
+      <input type="password" class="form-control form-signin-bottom" :placeholder="$t('sign.password')"
+        v-model="password" />
+      <div class="text-danger small mt-1" v-if="!valid_u" v-t="'sign.msg.valid_username'"></div>
+      <div class="text-danger small mt-1" v-if="!valid_p" v-t="'sign.msg.valid_password'"></div>
+      <div class="text-danger small mt-1" v-if="wrong_login != ''">
+        {{  wrong_login  }}
+      </div>
+      <div class="form-check mt-3 mb-3">
+        <input class="form-check-input" type="checkbox" value="" id="rememberme" style="float:none" v-model="remember">
+        <label class="form-check-label ms-1" for="rememberme" v-t="'sign.remember'"></label>
+      </div>
+      <button class="btn btn btn-primary btn-block" type="submit" style="width:50%" v-t="'sign.login'"></button>
     </form>
-</div>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
 import { validUsername, validPassword, SHA256, callRPC } from '@/utils'
 
-export default {
-    data() {
-        return { user_name: "", password: "", remember: false, invalid_u: false, invalid_p: false, wrong_login: "" }
-    },
-    methods: {
-        login() {
-            this.invalid_u = !validUsername(this.user_name)
-            this.invalid_p = !validPassword(this.password)
-            this.wrong_login = ""
-            if (!this.invalid_u && !this.invalid_p) {
-                var ppassword = SHA256(this.password)
-                callRPC('UserLogin', {
-                    user_name: this.user_name,
-                    password: ppassword,
-                    remember: this.remember,
-                }, (res) => {
-                    window.location.href = "/"
-                }, (res) => {
-                    this.wrong_login = res.data._error.message
-                })
-            }
-        },
-    }
+const user_name = ref(''),
+  password = ref(''),
+  remember = ref(false),
+  wrong_login = ref(''),
+  valid_u = computed(() => validUsername(user_name.value)),
+  valid_p = computed(() => validPassword(password.value));
+
+const login = () => {
+  wrong_login.value = ""
+  if (valid_u.value && valid_p.value) {
+    var ppassword = SHA256(password.value)
+    callRPC('UserLogin', {
+      user_name: user_name.value,
+      password: ppassword,
+      remember: remember.value,
+    }, (res) => {
+      window.location.href = "/"
+    }, (res) => {
+      wrong_login.value = res.data._error.message
+    })
+  }
 }
 </script>
 
